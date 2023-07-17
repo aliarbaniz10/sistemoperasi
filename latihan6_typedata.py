4fd1b89e4de99242867a7dc7e2e9486cc4eb70f0
aliarbaniz10 <137596693+aliarbaniz10@users.noreply.github.com>
2023-07-17 09:38:33 +0700

    Create latihan6_typedata.py

---
+++ latihan6_typedata.py
@@ -0,0 +1,46 @@
+#setting variabel identitas 
+nim   = int(input("Masukan NIM     : "))
+nama  = input("Masukan Nama    : ")
+kelas = int(input("Masukan Kelas   : "))
+prodi = input("Masukan Jurusan : ")
+
+name = input("Maukan Nama Bangunan : ")
+r = int(input("Jari-Jari : "))
+phi = 3.14
+
+#opeasi
+luas = phi*r*r
+
+#tapilan A
+print ("A")
+print ("================================")
+print ("      OPERASI ARITMAITKA        ")
+print ("================================")
+print ("")
+print ("Nim anda          :%d"%(nim))
+print ("Nama lengkap anda :%s"%(nama))
+print ("Kelas anda        :%d"%(kelas))
+print ("Progam Studi anda :%s"%(prodi))
+print ("")
+print ("--------------------------------")
+print ("   Bangunan %s"%(nama))
+print ("--------------------------------")
+print ("")
+print ("Nilai Jari-Jari :%d"%(r))
+print ("Luas Bangunan   :%d"%(luas))
+print ("")
+print ("================================")
+
+#list
+all= ["UNISS","di","belajar","pada","Mahasiswa","Pemrograan","ruang","lab","Algoritma","semester","topik","Data","Tipe","dan","dengan","Batang",2]
+#       0      1      2        3        4           5           6     7      8            9          10    11     12     13     14       15    16
+
+#tapilan B
+print ("B")
+print ("=====================================================================================================")
+print ("                                  SUSUN KATA SPOK         ")
+print ("=====================================================================================================")
+print ("")
+print (all[4],all[0],all[15],all[9],all[16],all[2],all[8],all[13],all[5],all[14],all[10],all[12],all[11],all[1],all[6],all[7])
+print ("")
+print ("=====================================================================================================")
